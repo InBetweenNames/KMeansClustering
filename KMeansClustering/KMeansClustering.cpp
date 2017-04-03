db108edd732fa38797bbdd5090ae0a9b0adcd152
Shane Peelar <lookatyouhacker@gmail.com>
2017-04-02 21:52:48 -0400

    Read in data

--- KMeansClustering/KMeansClustering.cpp
+++ KMeansClustering/KMeansClustering.cpp
@@ -2,7 +2,39 @@
 //
 
 #include <Eigen/Core>
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <tuple>
+#include <vector>
+
+std::pair<std::vector<std::string>, Eigen::MatrixXd> readVectors(const std::string& vectorFile)
+{
+	std::vector<std::string> ids;
+
+	std::ifstream vs{ vectorFile };
+	Eigen::Index M, N;
+	vs >> M >> N;
+
+	std::cout << "Document matrix is " << M << "x" << N << std::endl;
+
+	//Store matrix transposed such that rows are contiguous (Eigen is column major)
+	Eigen::MatrixXd vectors{ N, M };
+
+	for (Eigen::Index i = 0; i < M; i++)
+	{
+		std::string id;
+		vs >> id;
+		ids.emplace_back(id);
+		for (Eigen::Index j = 0; j < N; j++)
+		{
+			vs >> vectors(j, i);
+		}
+	}
+	
+	return { ids, vectors };
+
+}
 
 int __cdecl main(int argc, char* argv[])
 {
@@ -14,6 +46,16 @@ int __cdecl main(int argc, char* argv[])
 
 	const std::string vectorFile{ argv[1] };
 	const std::string outFile{ argv[2] };
+
+	std::cout << "Reading " << vectorFile << "..." << std::endl;
+
+	const auto docVecs = readVectors(vectorFile);
+
+	std::cout << "Determining optimal value of K" << std::endl;
+
+	std::cout << "Determining optimal clustering with K = ?" << std::endl;
+
+	std::cout << "Writing optimal clustering to " << outFile << std::endl;
 
     return 0;
 }
